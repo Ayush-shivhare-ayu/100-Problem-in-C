1982a0d6b65a45d3193c5dbcda09e35f29543206
Ayush shivhare <shivhare.ayush23@gmail.com>
2024-10-25 07:58:42 +0530

    Create Integer sum.c
    2. Calculate the sum of two integers

---
+++ Integer sum.c
@@ -0,0 +1,16 @@
+//2. Calculate the sum of two integers.//
+
+#include<stdio.h>
+int main ()
+{int a,b;
+
+ printf("ENTER THE FIRST NUMBER :- ");
+ scanf("%d",&a);
+
+ printf("ENTER THE SECOUND NUMBER :- ");
+ scanf("%d",&b);
+
+ printf("addition of the number is :- ", a*b );
+
+ return 0;
+}
