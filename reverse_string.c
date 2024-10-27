af8dbbf49f6bf8f705e9bc208c21eb6ecddd0c26
Ayush-shivhare-ayu <shivhare.ayush23@gmail.com>
2024-10-27 17:28:46 +0530

    REVERSE STRING

---
+++ reverse_string.c
@@ -0,0 +1,19 @@
+//6. Reverse a string
+// date- 27/10//24
+
+#include <stdio.h>
+
+int main(){
+    
+     char st [] ="ayush";
+     int i;
+
+     for(i=5;i>=0;i--){
+        printf("%c", st[i]);
+     }
+
+
+
+     return (0);
+
+}
