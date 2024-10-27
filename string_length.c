f9dde9b486a39842075c5817b8ea5f6062a5879f
Ayush-shivhare-ayu <shivhare.ayush23@gmail.com>
2024-10-27 19:42:37 +0530

    String_length

---
+++ string_length.c
@@ -0,0 +1,15 @@
+// 8. Find the length of a string
+// date--> 27/10/24
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    char s[] = "harrypotter";
+
+    printf("%d", strlen(s));
+
+    return (0);
+}
