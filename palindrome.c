a2a8b3e1f8276e0b6b23455fdaf481b1cda05430
Ayush-shivhare-ayu <shivhare.ayush23@gmail.com>
2024-10-29 13:20:08 +0530

    PALINDROME

---
+++ palindrome.c
@@ -0,0 +1,34 @@
+//how to check for  palindrome. 
+// reverse is same as original. example - 1221, racecar,54245 etc
+//date--->29/10/24
+
+#include <stdio.h>
+
+int main(){
+
+     int a, rem, q, result=0;
+
+     printf("enter the number :-");
+     scanf("%d", &a);
+
+     q=a;
+     while(q != 0);
+     {
+        rem =q%10;
+        result=result*10+ rem;
+        q=q/10;
+     }
+
+     if (result==a){
+        printf("THE NUMBER IS PALINDROME");
+     }
+
+     else{
+        printf("THE NUMBER IS NOT PALINDROME");
+     }
+
+
+
+     return (0);
+
+}
